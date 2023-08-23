f4578047488a00597390b2f7794b16802daa5511
Patrick Mahloy <patrick.mahloy@gmail.com>
2023-08-22 22:42:44 -0400

    cleaned up code, added styling to board

--- src/components/Kanban/Board.tsx
+++ src/components/Kanban/Board.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Dispatch, createContext, useContext, useReducer } from 'react';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -38,16 +36,15 @@ type ItemsReducerAction = {
   }
 }
 
-type TargetType // 2 types that are droppable
-  = 'name' | 'panelName' // list (white), parent (grey container)
+type TargetType
+  = 'name' | 'panelName' // Items | Panel
 
-interface ItemsContext {
+interface ItemsContextType {
   items: ItemsData;
   dispatch: Dispatch<ItemsReducerAction>;
 }
 
 const initialItems: ItemsData = {
-  // assumption: the are no duplicate values
   todo: [
     { id: 1, name: 'Take out the trash' },
     { id: 2, name: 'Cook dinner' }
@@ -62,31 +59,56 @@ const initialItems: ItemsData = {
   ],
 };
 
-const ItemsContext = createContext<ItemsContext>({
-  items: initialItems,
-  dispatch: () => { }, // I don't love this
-});
-
 const ItemTypes = {
   ITEM: 'item',
 };
 
+const ACTIONS = {
+  MOVE: 'move',
+};
+
+const ItemsContext = createContext<ItemsContextType>({
+  items: initialItems,
+  dispatch: () => { }
+});
+
+function itemsReducer(state: ItemsData, action: ItemsReducerAction) {
+  switch (action.type) {
+    case ACTIONS.MOVE:
+      const { id, source, target, targetType } = action.payload;
+      const newState: ItemsData = { ...state };
+      const position = newState[source].findIndex((item) => item.id === id);
+      let newTarget;
+
+      if (position === -1) return newState;
+
+      if (targetType === "panelName") {
+        newTarget = target.toLowerCase() as keyof ItemsListTypes;
+      } else newTarget = target;
+
+      // add to new list
+      newState[newTarget]?.push(newState[source][position]);
+
+      // remove from old list
+      newState[source]?.splice(position, 1);
+
+      return newState;
+    default:
+      return state;
+  }
+}
+
 function Item(props: ItemProps) {
-  console.log("Item props: ", props);
   const { sourceList, item } = props;
 
   const theItemsContext = useContext(ItemsContext);
   const { dispatch } = theItemsContext;
-  console.log("dispatch: ", theItemsContext);
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.ITEM,
     item: { id: item.id },
     end: (item, monitor) => {
       const dropResult = monitor.getDropResult<DropResult>();
-      console.log("useDrag > dropResult: ", dropResult);
-      // if null, dropResult no dispatch is called because nothing is dropped?
-
       let targetName, targetType;
 
       if (dropResult?.name) {
@@ -116,7 +138,7 @@ function Item(props: ItemProps) {
 
   return (
     <div
-      className={`item ${isDragging ? 'bg-red-500' : 'bg-white'}`}
+      className='p-2 my-2 bg-white rounded-sm drop-shadow-md'
       ref={drag}
     >
       {item.name}
@@ -128,25 +150,21 @@ function Items(props: ItemsProps) {
   const { name } = props;
   const { items } = useContext(ItemsContext);
 
-  const [{ isOver }, drop] = useDrop(() => ({
+  const [, drop] = useDrop(() => ({
     accept: ItemTypes.ITEM,
-    drop: (item, monitor) => {
-      // console.log("Drop > Item, Monitor: ", item, monitor);
+    drop: () => {
       return { name }
     },
     collect: (...args) => {
-      // console.log("useDrop > collect > args: ", args);
       const monitor = args[0];
       return {
-        isOver: monitor.isOver() // # are you over me or the parent (grey doesn't work)
+        isOver: monitor.isOver()
       }
     }
   }), [name, items]);
 
-  // console.log("Items :", items);
   return (
     <div
-      className={`items ${isOver ? 'bg-green-500' : ''}`}
       ref={drop}
     >
       {items[name].map((item) => (
@@ -160,81 +178,49 @@ function Items(props: ItemsProps) {
   );
 }
 
-const ACTIONS = {
-  MOVE: 'move',
-};
-
-function itemsReducer(state: ItemsData, action: ItemsReducerAction) {
-  switch (action.type) {
-    case ACTIONS.MOVE:
-      const { id, source, target, targetType } = action.payload;
-      const newState: ItemsData = { ...state };
-
-      console.log("itemsReducer action: ", action);
-
-      // find position is original list
-      const position = newState[source].findIndex((item) => item.id == id);
-
-      if (position === -1) return newState;
-
-      let newTarget;
-      if (targetType === "panelName") {
-        newTarget = target.toLowerCase() as keyof ItemsListTypes;
-      } else newTarget = target;
-
-      // add to new list
-      newState[newTarget]?.push(newState[source][position]);
-
-      // remove from old list
-      newState[source]?.splice(position, 1);
-
-      return newState;
-    default:
-      return state;
-  }
-}
-
 const Panel = ({ children, panelName }: any) => {
   const { items } = useContext(ItemsContext);
 
-  const [, drop] = useDrop(() => ({
+  const [{ isOver }, drop] = useDrop(() => ({
     accept: ItemTypes.ITEM,
-    drop: (item, monitor) => {
-      console.log("Panel useDrop drop: ", panelName);
+    drop: () => {
       return { panelName }
     },
     collect: (...args) => {
       const monitor = args[0];
-      console.log("Panel useDrop collect: ", args); 
       return {
-        isOver: monitor.isOver() // # are you over me or the parent (grey doesn't work)
+        isOver: monitor.isOver()
       }
     }
   }), [items, panelName]);
 
   return (
-    <div className='w-[300px] bg-gray-400 border-2 border-gray-500 rounded-md p-3' ref={drop}>{children}</div>
+    <div
+      className={`w-[300px] rounded-md p-3 ${isOver ? 'bg-blue-200' : 'bg-neutral-200'}`}
+      ref={drop}
+    >
+      {children}
+    </div>
   )
 };
 
 export default function Board() {
   const [items, dispatch] = useReducer(itemsReducer, initialItems);
-  console.log("Board items: ", items);
 
   return (
-    <main className='h-[94vh] flex flex-row justify-center gap-2 p-3'>
+    <main className='h-[94vh] flex flex-row justify-center gap-2 p-3 bg-blue-700'>
       <DndProvider backend={HTML5Backend}>
         <ItemsContext.Provider value={{ items, dispatch }}>
           <Panel panelName="Todo">
-            <h3 className='text-center'>Todo</h3>
+            <h3 className='text-2xl'>Todo</h3>
             <Items name="todo" />
           </Panel>
           <Panel panelName="Doing">
-            <h3 className='text-center'>Doing</h3>
+            <h3 className='text-2xl'>Doing</h3>
             <Items name="doing" />
           </Panel>
           <Panel panelName="Done">
-            <h3 className='text-center'>Done</h3>
+            <h3 className='text-2xl'>Done</h3>
             <Items name="done" />
           </Panel>
         </ItemsContext.Provider>
