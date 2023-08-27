6e70b98909f9d75b1484581edafeb158c09dd825
Patrick Mahloy <patrick.mahloy@gmail.com>
2023-08-27 04:57:51 -0400

    messages send, added some message div formatting

--- src/components/Messenger.tsx
+++ src/components/Messenger.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
 import { BsFillPersonFill } from 'react-icons/bs';
 import { RiSendPlaneFill } from 'react-icons/ri';
 
@@ -35,58 +36,81 @@ const fakeMessages: MessagesType[] = [
   {
     id: 3,
     flow: "incoming",
-    body: "Me neither! Now we can work any developer we want without worrying about timezone conflicts!"
+    body: "Me neither! Now we can work with any developer we want without worrying about timezone conflicts!"
   }
 ]
 
 export default function Messenger() {
-  const [_data, setData] = useState();
   const [messages, setMessages] = useState(fakeMessages);
+  const [draft, setDraft] = useState<string>('');
+  const [textareaheight, setTextareaheight] = useState(1); 
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api/')
-      .then(response => response.json())
-      .then(apiData => setData(apiData))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  const handleContentChange = (e: any) => {
+    if (e.target.value) { setDraft(e.target.value) };
+
+    const height = e.target.scrollHeight; 
+    const rowHeight = 25; 
+    const trows = Math.ceil(height / rowHeight) - 1; 
+    
+    if (trows && textareaheight) { 
+      
+      setTextareaheight(trows); 
+      
+    } 
+  };
+
+  const handleButtonClick = (e: any) => {
+    e.preventDefault();
+
+    if (draft !== '') {
+
+      const newMessage = {
+        id: messages.length - 1,
+        flow: "outgoing",
+        body: draft
+      }
+
+      setMessages((prev) => {
+        return [...prev, newMessage]
+      });
+    }
+
+    setDraft('');
+    setTextareaheight(1);
+  }
 
   return (
     <div className="box-border flex h-[calc(100vh-40px)]">
       <div className="min-w-[220px]">
         <p className="text-2xl my-5 text-center">Messages</p>
         <ul>
-          {threads.map((title, index) => {
-            // if (data) {
-            //   console.log("Thumbnail: ", data.results[0].picture.thumbnail);
-            //   console.log("First: ", data.results[0].name.first);
-            //   console.log("Last: ", data.results[0].name.last);
-            // }
-
+          {threads.map((name, index) => {
             return (
               <button key={index} className='flex items-center w-full pl-3 hover:bg-zinc-200'>
                 <BsFillPersonFill className='mr-2 p-1 border-2 rounded-full border-sky-600 text-4xl text-sky-600' />
                 <li className="my-3">
-                  {title}
+                  {name}
                 </li>
               </button>
             )
           })}
         </ul>
       </div>
-      <div className="w-full flex flex-col bg-zinc-200">
-        <div className='grow p-3 overflow-scroll'>
-          {messages.map(({ id, flow, body }) => {
+      <div className="w-full flex flex-col justify-end bg-zinc-200">
+        <div className='p-3 pb-0 overflow-scroll'>
+          {messages.map(({ id, flow, body }, index) => {
             return (
-              <div className={'flex ' + (flow === "outgoing"
+              <div key={index} className={'flex ' + (flow === "outgoing"
                 ? 'justify-end'
                 : 'justify-start'
               )}>
                 <div
                   key={id}
-                  className={'w-fit max-w-[45%] my-1 px-2 py-1 rounded-xl text-white ' + (flow === "outgoing"
-                    ? 'bg-sky-600'
-                    : 'bg-zinc-400'
-                  )}
+                  className={'max-w-[45%] my-1 px-2 py-1 rounded-xl text-white break-words '
+                    + (flow === "outgoing"
+                      ? 'bg-sky-600'
+                      : 'bg-zinc-500'
+                    )}
                 >
                   {body}
                 </div>
@@ -94,14 +118,20 @@ export default function Messenger() {
             );
           })}
         </div>
-        <div className="flex justify-center items-center p-3 bg-transparent">
+        <div className="flex justify-center items-center mb-1 p-3 bg-transparent">
 
           {/* Compose Message & Send Button */}
-          <div
-            contentEditable={true}
-            className="w-[30vw] max-w-xl mr-3 p-2 bg-white rounded-md outline-none"
+          <textarea
+            rows={textareaheight}
+            onInput={handleContentChange}
+            value={draft}
+            className="w-[30vw] max-w-xl mr-3 p-2 bg-white rounded-md outline-none resize-none"
           />
-          <button className='inline-block h-11 bg-sky-600 active:bg-sky-500 rounded-full p-3'>
+          <button
+            type='submit'
+            onClick={handleButtonClick}
+            className='inline-block h-11 bg-sky-600 active:bg-sky-500 rounded-full p-3'
+          >
             <RiSendPlaneFill className='text-xl text-white' />
           </button>
 
