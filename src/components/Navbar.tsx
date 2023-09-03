d466bec382c3867bffc76a5c0fa1b026e3521860
Patrick Mahloy <patrick.mahloy@gmail.com>
2023-09-03 01:17:17 -0400

    added dropdown menu for account settings and login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,50 @@
+import { useState } from "react";
+
 const navList = [
   'Team',
   'Schedule Sync',
   'Calendar',
   'Messenger',
-  'Kanban',
-  'Log Out'
+  'Kanban'
 ]
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(true);
+
+  const handleDropDown = () => {
+    isActive
+      ? setIsActive(false)
+      : setIsActive(true);
+  }
+
   return (
-    <div className="leading-[2.5rem] bg-sky-600 text-white">
-      {navList.map((link, index) => {
-        return (
-          <a key={index} className="inline-block mx-3 align-center" href="#">
-            {link}
-          </a>
-        )
-      })}
-    </div>
+    <>
+      <div className="flex justify-between items-center leading-[2.5rem] bg-sky-600 text-white pl-3">
+        <div>
+          {navList.map((link, index) => {
+            return (
+              <a key={index} className="inline-block mr-6 align-center" href="#">
+                {link}
+              </a>
+            )
+          })}
+        </div>
+        <a
+          href="#"
+          className="px-6"
+          onClick={() => handleDropDown()}
+        >
+          User name
+        </a>
+      </div>
+
+      {/* Dropdown Menu */}
+      <div className={isActive ? "hidden" : "absolute" + " right-0 flex justify-end"}>
+        <div className="w-fit px-6 py-2 bg-sky-600 text-white text-right">
+          <a href="#" className="block p-2 text-center">Settings</a>
+          <a href="#" className="block p-2 text-center">Log Out</a>
+        </div>
+      </div>
+    </>
   );
 }
